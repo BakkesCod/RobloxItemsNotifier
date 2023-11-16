70215fea2cda2bfbbe44bdbc38992e420b6b7288
BakkesCod <143846123+BakkesCod@users.noreply.github.com>
2023-11-15 19:48:08 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const request = require('request');
+
+const app = express()
+const UpdateHook = ""
+
+
+app.all('/', (req, res) => {
+    if (req.method == "HEAD") {
+        // Get Roblox Items Status
+        
+    } else {
+        res.send('Yo!')
+    }
+})
+app.listen(process.env.PORT || 3000)
